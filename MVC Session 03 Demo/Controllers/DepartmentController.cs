d994ac649e2218b052b644c29e86ccbd7503f8b7
michaelGerges2030 <michaelbayo858@gmail.com>
2024-03-21 16:58:03 +0200

    Department Controller - Create

--- MVC Session 03 Demo/Controllers/DepartmentController.cs
+++ MVC Session 03 Demo/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Route.C41.G03.BLL.Interfaces;
 using Route.C41.G03.BLL.Repositories;
+using Route.C41.G03.DAL.Models;
 
 namespace MVC_Session_03_Demo.Controllers
 {
@@ -20,5 +21,24 @@ namespace MVC_Session_03_Demo.Controllers
 			var departments = _departmentRepo.GetAll();	
 			return View(departments);
 		}
-	}
+
+		[HttpGet]
+		public IActionResult Create() 
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public IActionResult Create(Department department)
+        {
+           if (ModelState.IsValid) 
+			{
+				var count = _departmentRepo.Add(department);	
+				if (count > 0)
+					return RedirectToAction(nameof(Index));
+			}
+		   return View(department);	
+        }
+
+    }
 }
