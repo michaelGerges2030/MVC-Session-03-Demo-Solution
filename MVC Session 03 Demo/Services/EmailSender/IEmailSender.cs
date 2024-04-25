d4a749aebdca3824e9426c4b2b409b7e75d7d7c3
michaelGerges2030 <michaelbayo858@gmail.com>
2024-04-25 20:07:01 +0200

    Send Email

---
+++ MVC Session 03 Demo/Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MVC_Session_03_Demo.Services.EmailSender
+{
+	public interface IEmailSender
+	{
+		Task SendAsync(string from, string recipients, string subject, string body);
+	}
+}
